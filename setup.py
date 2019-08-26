22119438b65d926d8fcd1c9d5cb4b0a66b79238a
Viktor Persson <vph.persson@gmail.com>
2019-08-27 00:28:39 +0200

    Created setup.py.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='terminal_utils',
+    version='0.9',
+    url='https://github.com/vphpersson/terminal_utils',
+    author='vph',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: User Interfaces',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    python_requires='>=3.7'
+)
